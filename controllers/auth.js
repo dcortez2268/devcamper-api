c40d4b21e7d45921313be11a5a8bcb6be81fcf96
Dominick Cortez <jdcortez2268@gmail.com>
2022-11-10 14:58:37 -0800

    fix sendTokenResponse bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,3 +54,26 @@ exports.getMe = asyncHandler(async (req, res, next) => {
         data: user,
     })
 })
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+    const token = user.getSignedJwtToken()
+
+    res.status(200).json({ success: true, token })
+    const options = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true
+    }
+
+    res.status(statusCode).cookie('token', token, options).json({
+        success: true,
+        token,
+    })
+}
